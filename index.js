bc2e7a4edd85dff19005c2d99135f99fb3ab7247
Dashshare <daianhnguyen206@gmail.com>
2026-05-04 22:24:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('Một thiết bị đã kết nối:', socket.id);
+  console.log('Thiết bị kết nối:', socket.id);
+
   socket.on('signal', (data) => {
     socket.broadcast.emit('signal', data);
   });
-});
 
+  socket.on('disconnect', () => {
+    console.log('Thiết bị đã thoát:', socket.id);
+  });
+});
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server đang chạy tại port ${PORT}`);
+  console.log(`Server chạy tại: ${PORT}`);
 });
