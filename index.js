8df04a1a759d8ca620c1d8b76f6e7705e2e982a8
Dashshare <daianhnguyen206@gmail.com>
2026-05-04 22:37:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: { origin: "*", methods: ["GET", "POST"] }
 });
-
 io.on('connection', (socket) => {
   socket.on('join-room', (roomId) => {
     socket.join(roomId);
-    console.log(`ID ${socket.id} vào phòng: ${roomId}`);
+    socket.to(roomId).emit('user-joined'); 
   });
-
   socket.on('signal', (data) => {
     socket.to(data.roomId).emit('signal', data);
+  });
+  socket.on('send-chat', (data) => {
+    io.to(data.roomId).emit('receive-chat', data);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server đang chạy...`));
+http.listen(PORT, () => console.log("Server Stream & Chat đang chạy..."));
